947937ff20e77116f47176efdc0cc7ec30b33045
sarathsuryas <sarathsuryasss48@gmail.com>
2026-04-03 06:13:26 +0530

    cicd setup

---
+++ verify-env.js
@@ -0,0 +1,3 @@
+console.log('PORT:', process.env.PORT ? 'Set' : 'Not Set');
+console.log('MONGODB_CONNECTION_STRING:', process.env.MONGODB_URI ? 'Set' : 'Not Set');
+console.log('CORS_ORIGIN:', process.env.CORS_ORIGIN ? 'Set' : 'Not Set');
